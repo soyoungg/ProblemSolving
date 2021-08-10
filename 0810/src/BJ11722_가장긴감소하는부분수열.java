240cd7384113c7fcc3cebd5b28a1e4622f6ce120
고소영 <kosoyoung123@gmail.com>
2021-08-10 23:08:02 +0900

    BJ11722_가장 긴 감소하는 부분 수열

---
+++ 0810/src/BJ11722_가장긴감소하는부분수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ11722_가장긴감소하는부분수열 {
+	static int N, res;
+	static int arr[];
+	static int dp[];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		
+		arr = new int [N + 1];
+		dp = new int [N + 1];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = N; i > 0; i--) {
+			dp[i] = 1;
+			for (int j = N; j > i; j--) {
+				if(arr[j] < arr[i])
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+			}
+			res = Math.max(res, dp[i]);
+		}
+		
+		System.out.println(res);
+	}
+}
